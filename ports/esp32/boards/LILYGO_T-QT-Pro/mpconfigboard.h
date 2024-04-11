600b90db4fad93b9561fc771e5c5ab42380ccdc4
ubi de feo <me@ubidefeo.com>
2024-04-11 06:32:21 +0200

    ports/esp32: LILYGO-T-QT-PRO fixed wrong pin order for I2C(0).
    Signed-off-by: ubi de feo <me@ubidefeo.com>

--- ports/esp32/boards/LILYGO_T-QT-Pro/mpconfigboard.h
+++ ports/esp32/boards/LILYGO_T-QT-Pro/mpconfigboard.h
@@ -7,8 +7,8 @@
 // Enable UART REPL for modules that have an external USB-UART and don't use native USB.
 #define MICROPY_HW_ENABLE_UART_REPL (1)
 
-#define MICROPY_HW_I2C0_SCL (43)
-#define MICROPY_HW_I2C0_SDA (44)
+#define MICROPY_HW_I2C0_SCL (44)
+#define MICROPY_HW_I2C0_SDA (43)
 
 #define MICROPY_BOARD_STARTUP LILYGO_TQT_PRO_ESP32_board_startup
 void LILYGO_TQT_PRO_ESP32_board_startup(void);
